902f6cfec2ff3e30c7613051ec984ba0b039a7e3
Ishwar <ishwar.t@medkart.in>
2025-02-04 10:52:47 +0530

    Add ProductDraft and ProductPublished models and migrate, controllers, requests, factories, and policies only made

---
+++ app/Http/Controllers/ProductDraftController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductDraft;
+use App\Http\Requests\StoreProductDraftRequest;
+use App\Http\Requests\UpdateProductDraftRequest;
+
+class ProductDraftController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductDraftRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductDraft $productDraft)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductDraft $productDraft)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductDraftRequest $request, ProductDraft $productDraft)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductDraft $productDraft)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_02_04_045258_create_product_drafts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_drafts', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name', 255)->unique();
+            $table->string('manufacturer_name', 255);
+            $table->decimal('sales_price', 10, 2);
+            $table->decimal('mrp', 10, 2);
+            $table->text('molecule_string');
+            $table->boolean('is_banned')->default(false);
+            $table->boolean('is_discontinued')->default(false);
+            $table->boolean('is_assured')->default(false);
+            $table->boolean('is_refridgerated')->default(false);
+            $table->boolean('is_published')->default(false);
+            $table->timestamps();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
+            $table->boolean('is_active')->default(true);
+            $table->foreignId('deleted_by')->nullable()->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_drafts');
+    }
+};
